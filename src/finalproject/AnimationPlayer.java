82f991e82a34b4ca5595b5a894cf24d8032e07ce
Evan <Evan@DESKTOP-7K7AU84.phub.net.cable.rogers.com>
2022-04-02 20:52:11 -0400

    window

--- src/finalproject/AnimationPlayer.java
+++ src/finalproject/AnimationPlayer.java
@@ -1,6 +1,25 @@
 package finalproject;
 
-public class AnimationPlayer {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class AnimationPlayer extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        Pane pane = new Pane();
+        
+        Scene scene = new Scene(pane, 1600, 900, Color.WHITESMOKE);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Animation Player");
+        
+        primaryStage.show();
+        
+    }
     
     void loadAnimationFromFile(String fileName) {
         
@@ -8,6 +27,8 @@ public class AnimationPlayer {
     
     void run() {
         
+        launch();
+    
     }
     
 }
